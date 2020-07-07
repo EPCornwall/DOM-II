2e1995c435fac385076c104934cbedc73d81ac70
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-07-07 18:04:08 -0500

    oops I didnt commit all day

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-// Your code goes here
+// element.addEventListener('click', (event) => {//Handle event});
+// create 10 [unique event listeners
+// * [1 ] `mouseover`
+// * [2 ] `keydown`
+// * [3 ] `click`
+// * [4 ] `mouseout`
+// * [5 ] `load`
+// * [6 ] `dblclick`
+// * [7 ] ``
+
+const funBusClick = document.querySelector('.logo-heading')
+funBusClick.addEventListener('click', ()=>{
+    funBusClick.style.color = 'pink';
+})
+
+const bannerHover = document.querySelector('.nav a')
+bannerHover.addEventListener('mouseover', ()=>{
+    bannerHover.style.color = 'pink';
+})
+
+document.addEventListener('keydown', function (event){
+    if (event.key === 'Escape'){
+        document.body.style.backgroundColor = 'blue';
+    }
+} )
+
+const bannerMouseout = document.querySelector('.nav a')
+bannerMouseout.addEventListener('mouseout', ()=>{
+    bannerMouseout.style.color = 'red';
+})
+
+window.addEventListener('load', () => {
+    console.log('It lives')
+  })
+
+funBusClick.addEventListener('dblclick', ()=>{
+    funBusClick.style.color = 'black';
+})
+
+
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+
+
+// * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+
+
+
